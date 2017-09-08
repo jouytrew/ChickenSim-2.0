820e1f7fbfab6672cb5a4e2050b6ee7b6051579c
Benjamin Wong <benjamin.wong@alumni.ubc.ca>
2017-09-08 04:05:17 -0700

    Version works
    Reset to here if needed.

--- ChickenSim 2.0/FPrinter.cpp
+++ ChickenSim 2.0/FPrinter.cpp
@@ -3,11 +3,11 @@
 
 void FPrinter::PrintMoveHistory(FMoveHistory::Move MoveHistory[MOVES][PLAYERS])
 {
-	std::cout << "Hello from PrintMoveHistory inside FPrinter Printer" << std::endl;
-	printf("    1 2\n-------\n");
+
+	std::cout << "    1 2\n-------" << std::endl;
 	for (int i = 0; i < MOVES; i++)
 	{
-		printf("%3.i %s %s\n", i + 1, (MoveHistory[i][P1] ? "D" : "C"), (MoveHistory[i][P2] ? "D" : "C"));
+		printf("%3.i %s %s\n", i + 1, MoveHistory[i][P1] ? "D" : "C", MoveHistory[i][P2] ? "D" : "C"); // if the value stored in the array is 0 print C, if 1 print D
 		std::cout << std::endl;
 	}
 
